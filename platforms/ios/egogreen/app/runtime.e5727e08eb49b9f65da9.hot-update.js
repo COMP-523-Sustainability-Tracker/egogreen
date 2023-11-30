84f2dabdb9d98cf22da5b5e2a23febd16f16a0b3
baonhivu <baonhi@email.unc.edu>
2023-11-30 01:01:31 -0500

    adding to ios

---
+++ platforms/ios/egogreen/app/runtime.e5727e08eb49b9f65da9.hot-update.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.id = "runtime";
+exports.ids = null;
+exports.modules = {};
+exports.runtime =
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("cd6b502f399cbc405373")
+/******/ })();
+/******/ 
+/******/ }
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicnVudGltZS5lNTcyN2UwOGViNDliOWY2NWRhOS5ob3QtdXBkYXRlLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7O1VBQUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9lZ29ncmVlbi93ZWJwYWNrL3J1bnRpbWUvZ2V0RnVsbEhhc2giXSwic291cmNlc0NvbnRlbnQiOlsiX193ZWJwYWNrX3JlcXVpcmVfXy5oID0gKCkgPT4gKFwiY2Q2YjUwMmYzOTljYmM0MDUzNzNcIikiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
